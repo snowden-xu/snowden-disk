b7c77022199add1333c01597c6b92c588ef246ef
snowden.xu <snowden.xu@gmail.com>
2025-01-24 16:28:32 +0800

    feat: 接入antd

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,13 +1,51 @@
+import { Button, Space, Table } from 'antd';
 import React, { useState } from 'react';
 
 const App = () => {
   const [count, setCount] = useState(0);
+  const dataSource = [
+    {
+      key: '1',
+      name: '胡彦斌',
+      age: 32,
+      address: '西湖区湖底公园1号',
+    },
+    {
+      key: '2',
+      name: '胡彦祖',
+      age: 42,
+      address: '西湖区湖底公园1号',
+    },
+  ];
+
+  const columns = [
+    {
+      title: '姓名',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: '年龄',
+      dataIndex: 'age',
+      key: 'age',
+    },
+    {
+      title: '住址',
+      dataIndex: 'address',
+      key: 'address',
+    },
+  ];
   return (
     <div>
       黄老哥牛逼
+      <Table dataSource={dataSource} columns={columns} />
       <p>{count}</p>
-      <button onClick={() => setCount((pre) => pre + 1)}>+</button>
-      <button onClick={() => setCount((pre) => pre - 1)}>-</button>
+      <Space>
+        <Button onClick={() => setCount((pre) => pre + 1)}>+</Button>
+        <Button type="primary" onClick={() => setCount((pre) => pre - 1)}>
+          -
+        </Button>
+      </Space>
     </div>
   );
 };
