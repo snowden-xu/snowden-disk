0b9ef190a2a2b86c8e072c63293626d0871aef4c
snowden.xu <snowden.xu@gmail.com>
2025-01-24 15:58:20 +0800

    feat: 配置React

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+
+const App = () => {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      黄老哥牛逼
+      <p>{count}</p>
+      <button onClick={() => setCount((pre) => pre + 1)}>+</button>
+      <button onClick={() => setCount((pre) => pre - 1)}>-</button>
+    </div>
+  );
+};
+
+export default App;
--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -1,4 +1,8 @@
-import { defineConfig } from 'vite';
-
+import { defineConfig } from "vite";
 // https://vitejs.dev/config
-export default defineConfig({});
+export default defineConfig({
+  root: "src/renderer",
+  build: {
+    outDir: ".vite/build/renderer",
+  },
+});
